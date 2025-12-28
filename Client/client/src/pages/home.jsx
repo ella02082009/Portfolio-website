64d729f600e202da82c88acadd01fa706759d304
Emmanuella Orjih <emmanuellaorjih08@gmail.com>
2025-12-28 21:43:41 +0100

    Fix case sensitivity in project image import path

--- Client/client/src/pages/home.jsx
+++ Client/client/src/pages/home.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import profileImg from '../assets/profile.png';
 import project1 from '../assets/Project1.jpg';
-import project2 from '../assets/Project2.jpg';
+import project2 from '../assets/project2.jpg';
 import project3 from '../assets/Project3.jpg';
 import project4 from '../assets/Project4.jpg';
 import {FaFigma,FaReact,FaJs} from "react-icons/fa";
